b3bcdff80120073176fadd51e8a7d8091f637c29
obhassan1 <you@example.com>
2026-08-11 19:53:25 +0300

    api fix"

--- Client/JosHouseOfSubs/src/environments/environment.prod.ts
+++ Client/JosHouseOfSubs/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // Client/JosHouseOfSubs/src/environments/environment.prod.ts
 export const environment = {
   production: true,
-apiUrl: 'https://joshouseofsubs.onrender.com/api/v1'
+  apiUrl: 'https://joshouseofsubs-api.onrender.com/api/v1'
 };
